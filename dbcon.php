07af54c7083f5cc738aa128d07e5877e58eedd2d
Muhammad Azeem <azeemmuhammad98@gmail.com>
2021-06-02 19:04:00 -1200

    first commit

--- dbcon.php
+++ dbcon.php
@@ -2,8 +2,8 @@
 session_start();
 // error_reporting(0);
 //connection
-$con = new mysqli("localhost", "root", "", "covid");
-// $con = new mysqli("localhost", "u352871344_covid", "Covid@digital123", "u352871344_covid");
-if($con->connect_error){
-	die('failed to connect'.$con->connect_error);
-}
+// $con = new mysqli("localhost", "root", "", "covid");
+// // $con = new mysqli("localhost", "u352871344_covid", "Covid@digital123", "u352871344_covid");
+// if($con->connect_error){
+// 	die('failed to connect'.$con->connect_error);
+// }
