29e9c952be12e2bdbf48a2c29ac5ce1926c8ef6d
Muhammad Azeem <azeemmuhammad98@gmail.com>
2021-06-02 22:02:26 -1200

    footer

--- index.php
+++ index.php
@@ -11,14 +11,94 @@ index();
                 <p class="bold-tdb6">Make a free milkshake website on your phone in minutes</p>
                 <br><img src="img/app_ios_v1.png" class="h-px-tdb40" alt="ios app"> <img src="img/app_android_v1.png" class="h-px-tdb40" alt="ios app">
                 <br><br>
-            <button class="btn-tdb b-tdb-red9 c-tdb-red9 btn-tdb1">Watch this <i class="fa fa-play font-tdb12"></i></button>
-            <br><br><br><br><br><br><i class="fas font-tdb25 fa-angle-double-down c-tdb-red8"></i>
-            <br><br><br>
-            
+                <button class="btn-tdb b-tdb-red9 c-tdb-red9 btn-tdb1">Watch this <i class="fa fa-play font-tdb12"></i></button>
+                <br><br><br><br><br><br><i class="fas font-tdb25 fa-angle-double-down c-tdb-red8"></i>
+                <br><br><br>
+
             </div>
         </div>
     </div>
+</section>
 
+<section class="b-tdb-red9">
+
+    <div class="tdb-super-container c-tdb-red8">
+        <div class="tdb-md-row padding-tdb30">
+            <div class="tdb-md-12">
+                <h1 class="bold-tdb8 font-tdb35 c-tdb-red9 center">Popular Packages</h1>
+            </div>
+            <div class="tdb-md-4 margin-top-tdb10 position-r-tdb">
+                <div class="b-tdb-red8 h-per-tdb100 s-center-tdb2 padding-tdb20 margin-tdb10 bold-tdb6 btn-tdb2" style="min-height:130px">
+                    <h2 class="s-center1-tdb1 position-a-tdb btn-tdb1 font-tdb15 left-tdb0 b-tdb-red9 padding-tdb15 c-tdb-white top-tdb0 bold-tdb8">
+                        91% Off</h2><br><br><br>
+
+                    <h2 class="bold-tdb5 c-tdb-red8 margin-tdb0 font-tdb23">Healthy India 2021 Full Body Checkup</h2><br>
+                    Includes<br><br>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        Blood Glucose Fasting
+                    </li>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        Complete Hemogram
+                    </li>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        HbA1c
+                    </li>
+                    <br>
+                    <p class="c-tdb-red8">
+                        <del>₹7733</del> <span class="bold-tdb8 font-tdb30 padding-left-tdb10 c-tdb-red8">₹ 699
+                    </p>
+                    <br><button value="1" class="package_btn btn-tdb btn-tdb1 b-tdb-red9 c-tdb-red9 ">Book Now</button>
+                </div>
+            </div>
+            <div class="tdb-md-4 margin-top-tdb10 position-r-tdb">
+                <div class="b-tdb-red8 h-per-tdb100 s-center-tdb2 padding-tdb20 margin-tdb10 bold-tdb6 btn-tdb2" style="min-height:130px">
+                    <h2 class="s-center1-tdb1 position-a-tdb btn-tdb1 font-tdb15 left-tdb0 b-tdb-red9 padding-tdb15 c-tdb-white top-tdb0 bold-tdb8">
+                        91% Off</h2><br><br><br>
+
+                    <h2 class="bold-tdb5 c-tdb-red8 margin-tdb0 font-tdb23">Healthy India 2021 Full Body Checkup</h2><br>
+                    Includes<br><br>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        Blood Glucose Fasting
+                    </li>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        Complete Hemogram
+                    </li>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        HbA1c
+                    </li>
+                    <br>
+                    <p class="c-tdb-red8">
+                        <del>₹7733</del> <span class="bold-tdb8 font-tdb30 padding-left-tdb10 c-tdb-red8">₹ 699
+                    </p>
+                    <br><button value="1" class="package_btn btn-tdb btn-tdb1 b-tdb-red9 c-tdb-red9 ">Book Now</button>
+                </div>
+            </div>
+            <div class="tdb-md-4 margin-top-tdb10 position-r-tdb">
+                <div class="b-tdb-red8 h-per-tdb100 s-center-tdb2 padding-tdb20 margin-tdb10 bold-tdb6 btn-tdb2" style="min-height:130px">
+                    <h2 class="s-center1-tdb1 position-a-tdb btn-tdb1 font-tdb15 left-tdb0 b-tdb-red9 padding-tdb15 c-tdb-white top-tdb0 bold-tdb8">
+                        91% Off</h2><br><br><br>
+
+                    <h2 class="bold-tdb5 c-tdb-red8 margin-tdb0 font-tdb23">Healthy India 2021 Full Body Checkup</h2><br>
+                    Includes<br><br>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        Blood Glucose Fasting
+                    </li>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        Complete Hemogram
+                    </li>
+                    <li class="padding-tdb5 btn-tdb1 margin-bottom-tdb5">
+                        HbA1c
+                    </li>
+                    <br>
+                    <p class="c-tdb-red8">
+                        <del>₹7733</del> <span class="bold-tdb8 font-tdb30 padding-left-tdb10 c-tdb-red8">₹ 699
+                    </p>
+                    <br><button value="1" class="package_btn btn-tdb btn-tdb1 b-tdb-red9 c-tdb-red9 ">Book Now</button>
+                </div>
+            </div>
+            
+        </div>
+        </br></br></br>
 </section>
 
 <?php include 'static/footer.php'; ?>
