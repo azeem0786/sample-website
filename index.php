4f185264feb037f8c87c1dabc6fa50866705cf13
Muhammad Azeem <azeemmuhammad98@gmail.com>
2021-06-04 12:24:55 -1200

    index

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 include 'static/functions.php';
 index();
 ?>
-<section class="b-tdb-red8 center c-tdb-red8 letter-tdb1" style="min-height:100vh">
+
+<header class="b-tdb-red8 center c-tdb-red8 letter-tdb1" style="min-height:100vh">
     <div class='tdb-super-container position-r-tdb'>
         <div class="tdb-md-row">
             <div class="tdb-md-12 padding-tdb40">
@@ -18,14 +19,146 @@ index();
             </div>
         </div>
     </div>
+</header>
+
+
+<section class="b-tdb-white c-tdb-black">
+    <div class="tdb-super-container">
+        <div class="tdb-md-row padding-tdb30">
+            <div class="tdb-md-6">
+                <h2 class="bold-tdb8 font-tdb35 c-tdb-red8">Turn your link in bio into a free Milkshake website.</h2>
+                <h2>Say more, sell more, share more</h2>
+                <p>Connect your followers to all you offer from your link in bio with a Milkshake website.</p>
+                <h2>Make it happen</h2>
+                <p>Use a Milkshake website to start, launch or grow business ideas, passion projects and promotions.</p>
+                <h2>Fun, easy, beautiful, instant</h2>
+                <p>Make and update a Milkshake website in minutes. No desktops, design or web skills needed!</p>
+            </div>
+            <div class="tdb-md-6">
+                <br><br><br>
+                <img src="img/img1.jpg" alt="" style="transform: rotate(-20deg);width:32%">
+                <img src="img/img2.jpg" alt="" style="transform: rotate(20deg);width:32%">
+                <img src="img/img3.jpg" alt="" style="transform: rotate(-20deg);width:32%">
+                <br><br>
+            </div>
+        </div>
+    </div>
 </section>
 
-<section class="b-tdb-red9">
+<section class="padding-top-tdb40 padding-bottom-tdb40">
+    <div class="tdb-super-container">
+        <div class="tdb-md-row">
+            <div class="tdb-md-12">
+                <div class="center bold-tdb2 font-tdb20 letter-tdb2 c-tdb-gray6">AS SEEN ON</div>
+            </div>
+            <div class="tdb-md-12">
+                <div class="tdb-md-row center">
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/bandt-hover-1.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/creativeimpact-hover-1.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/frankie-hover-v2.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/freelancingfemales-hover.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center center">
+                        <img src="img/logos/futurewomen-hover-1.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/invision-hover-1.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/lifehacker-hover.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                    <div class="tdb-md-3 center">
+                        <img src="img/logos/voguecodes-hover-1.png" class="mylogo h-px-tdb70" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <style>
+        .mylogo {
+            filter: grayscale(80%);
+            transition: 0.2s;
+            opacity: 0.5;
+        }
 
+        .mylogo:hover {
+            filter: grayscale(0%);
+            opacity: 1;
+        }
+    </style>
+</section>
+
+<section class="padding-top-tdb40 padding-bottom-tdb40 b-tdb-pink1">
+    <div class="tdb-super-container">
+        <div class="tdb-md-row center">
+            <div class="tdb-md-12">
+                <h2 class="center c-tdb-red8 bold-tdb6 font-tdb30">It’s easy with the Milkshake app.</h2>
+            </div>
+            <div class="tdb-md-6 center">
+                <img src="img/img4.png" class="w-per-tdb60" alt="">
+            </div>
+            <div class="tdb-md-6 position-r-tdb bottom-tdb0" style="min-height:400px">
+                <div class="position-a-tdb bottom-tdb0 left padding-tdb40">
+               <h2>#Pick a Card</h2>
+               <p>Cards are the website pages of your Milkshake website. You can swipw between each card like an Instagram Story</p>
+               <br>
+               <i class="fa fa-circle c-tdb-red8 font-tdb10"></i>
+               <i class="fa fa-circle c-tdb-gray6 font-tdb8"></i>
+               <i class="fa fa-circle c-tdb-gray6 font-tdb8"></i>
+               <i class="fa fa-circle c-tdb-gray6 font-tdb8"></i>
+               <i class="fa fa-circle c-tdb-gray6 font-tdb8"></i>
+               
+               <br><br><br><br><br>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="padding-top-tdb40 padding-bottom-tdb40 b-tdb-red8">
+    <div class="tdb-super-container">
+        <div class="tdb-md-row center">
+            <div class="tdb-md-6 center">
+            <img src="img/photos/img1.jpg" class="myphoto" alt="" style="transform: rotate(20deg);width:200px">
+            <img src="img/photos/img2.jpg" class="myphoto" alt="" style="transform: rotate(-10deg);width:250px"><br>
+            <img src="img/photos/img3.jpg" class="myphoto" alt="" style="transform: rotate(10deg);width:200px">
+            <img src="img/photos/img4.jpg" class="myphoto" alt="" style="transform: rotate(-30deg);width:180px;">
+            </div>
+
+            <div class="tdb-md-6 center">
+                <h2 class="bold-tdb8 font-tdb45 c-tdb-red8">Made for</h2>
+                <p class="bold-tdb6 font-tdb40 c-tdb-red8 margin-top-tdb0">creators<br>
+game changers<br>
+trendsetters<br>
+go-getters<br>
+quiet achievers<br>
+thought leaders<br>
+
+you</p>
+            </div>
+        </div>
+    </div>
+    
+    <style>
+        .myphoto {
+            transition: 0.2s;
+            border-radius:1em;
+        }
+    </style>
+</section>
+<!-- package -->
+<section class="b-tdb-pink1">
     <div class="tdb-super-container c-tdb-red8">
         <div class="tdb-md-row padding-tdb30">
             <div class="tdb-md-12">
-                <h1 class="bold-tdb8 font-tdb35 c-tdb-red9 center">Popular Packages</h1>
+                <h1 class="bold-tdb8 font-tdb35 c-tdb-red8 center">Popular Packages</h1>
             </div>
             <div class="tdb-md-4 margin-top-tdb10 position-r-tdb">
                 <div class="b-tdb-red8 h-per-tdb100 s-center-tdb2 padding-tdb20 margin-tdb10 bold-tdb6 btn-tdb2" style="min-height:130px">
@@ -96,10 +229,14 @@ index();
                     <br><button value="1" class="package_btn btn-tdb btn-tdb1 b-tdb-red9 c-tdb-red9 ">Book Now</button>
                 </div>
             </div>
-            
+
         </div>
         </br></br></br>
+
 </section>
+
+
+
 
 <?php include 'static/footer.php'; ?>
 </body>
